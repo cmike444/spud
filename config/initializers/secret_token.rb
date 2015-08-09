9bde795a1c43aafd8102c8b71c87713359aead0a
Chris Mikelson <chris.mikelson@640labs.com>
2015-08-09 12:59:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spud::Application.config.secret_key_base = 'c8f147c624e54fb0ffeef9b4ca2630ae85c554a45aab18ad8aa75c601fd5f7f2280fc3b370757fe1a69ef00a8fad29622e94e72eb17af3d9dfe614b2bc12ecbd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spud::Application.config.session_store :cookie_store, key: '_spud_session'
